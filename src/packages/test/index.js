9e62d7f1065d45b8a317dbe12cce9746bebbf959
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-05-02 00:08:04 +0530

    created a package to convert css classes to monoatomic classes

---
+++ src/packages/test/index.js
@@ -0,0 +1,27 @@
+// const convertCssToMonatomic = require('css-to-monatomic');
+
+// const css = `
+//   .text-center {
+//     text-align: center;
+//   }
+//   .text-bold {
+//     font-weight: bold;
+//   }
+// `;
+
+// const monatomicCss = convertCssToMonatomic(css);
+// console.log(monatomicCss);
+
+const generateTheme = require('../theme/theme-generator');
+
+// Define your theme object
+const myTheme = {
+  buttonColors: ['white', 'green', 'red', 'yellow'],
+  buttonBackgroundColor: 'blue',
+  // Add more theme properties as needed
+};
+
+
+// Generate theme stylesheet
+generateTheme(myTheme);
+
